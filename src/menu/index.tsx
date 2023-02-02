28fc217134952bccdd159d8bd9ae3c07d3ab2416
tianjia <970081659@qq.com>
2023-02-02 15:24:05 +0800

    test

--- src/menu/index.tsx
+++ src/menu/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { DatePicker } from 'antd';
 
-const App = () => {
+const Test = () => {
   return <DatePicker />;
 };
 
-export default App;
+export default Test;
